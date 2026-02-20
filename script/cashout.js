5628c620c014dc7ad6b5de75b2a1458492949dfd
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-02-21 00:21:56 +0600

    Create machine js file and made the functionality code shorter

--- script/cashout.js
+++ script/cashout.js
@@ -1,35 +1,68 @@
 document.getElementById('btn-withdraw')
     .addEventListener('click', function () {
-        // 1. get the number and validate
-        const inputNumber = document.getElementById('inputNumber')
-        const number = inputNumber.value
-        console.log(number);
+        // 1. get the number
+        const number = getValueFromInput('inputNumber')
         if (number != '01721132610') {
             alert('Invalid Number')
             return;
         }
-
-        // 2. get the amount
-        const amountInput = document.getElementById('inputAmount')
-        const amount = amountInput.value
-        console.log(amount);
-        // 3. get the current balance
+        // 2. get the amount and calculate
+        const amount = getValueFromInput('inputAmount')
         const balance = document.getElementById('balance')
         const balanceValue = balance.innerText
-
-        // 4. calculate 
         const newBalance = Number(balanceValue) - Number(amount)
         if (newBalance < 0) {
-            alert('invalid Ammount')
-            return
-        }
-        // 5. get the password and validate
-        const inputPin = document.getElementById('inputPin')
-        const pin = inputPin.value
-        if (pin != '1234') {
-            alert('Invalid Pin')
+            alert('Invalid Balance')
             return;
-        } 
+        }
+
+        // 3.get Pin & validate
+        const pin = getValueFromInput('inputPin')
+        if (pin != '1234') {
+            alert('invalid pin')
+            return;
+        }
         balance.innerText = newBalance
-        alert('CashOut successful')
+        alert('Cash out sucessful')
+
+
+
+
     })
+
+
+// document.getElementById('btn-withdraw')
+//     .addEventListener('click', function () {
+//         // 1. get the number and validate
+//         const inputNumber = document.getElementById('inputNumber')
+//         const number = inputNumber.value
+//         console.log(number);
+//         if (number != '01721132610') {
+//             alert('Invalid Number')
+//             return;
+//         }
+
+//         // 2. get the amount
+//         const amountInput = document.getElementById('inputAmount')
+//         const amount = amountInput.value
+//         console.log(amount);
+//         // 3. get the current balance
+//         const balance = document.getElementById('balance')
+//         const balanceValue = balance.innerText
+
+//         // 4. calculate
+//         const newBalance = Number(balanceValue) - Number(amount)
+//         if (newBalance < 0) {
+//             alert('invalid Ammount')
+//             return
+//         }
+//         // 5. get the password and validate
+//         const inputPin = document.getElementById('inputPin')
+//         const pin = inputPin.value
+//         if (pin != '1234') {
+//             alert('Invalid Pin')
+//             return;
+//         }
+//         balance.innerText = newBalance
+//         alert('CashOut successful')
+//     })
---
+++ script/machine.js
@@ -0,0 +1,7 @@
+function getValueFromInput (id){
+    const input = document.getElementById(id)
+    const value = input.value
+    console.log(value);
+    return value;
+    
+}
